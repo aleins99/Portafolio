7967cd3cc555946c26a0742208b2d19e8a03caa7
Alejandro Marin <alexword1893@gmail.com>
2023-09-04 00:49:24 -0400

    a bug fixed when tapping on the phone did not work

--- src/components/FlippingCard.jsx
+++ src/components/FlippingCard.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/components/FlippingCard.css";
 
 const FlippingCard = ({ imageSrc, description }) => {
+  const [showDescription, setShowDescription] = useState(false);
+
+  const handleTouch = () => {
+    setShowDescription(!showDescription);
+  };
   return (
-    <div className="card-container">
+    <div
+      className={`card-container  ${showDescription ? "flipped" : ""}`}
+      onTouchStart={handleTouch}
+      onTouchEnd={handleTouch}
+    >
       <div className="card-inner">
         <div className="card-front">
           <img src={imageSrc} alt="Imagen" />
