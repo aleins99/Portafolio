3aa11fcf7120b41abbf9cde4bb611a52351e703a
Alejandro Marin <alexword1893@gmail.com>
2023-09-06 00:47:32 -0400

    added visual enhancements for projects and changed fonts

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,9 +4,18 @@ import FlippingCard from "./FlippingCard";
 const ProjectCard = ({ title, description, technologies, link, image }) => {
   return (
     <div className="project-card">
-      <h3>{title}</h3>
-      <FlippingCard imageSrc={image} description={description} />
-      <p>Tecnologías utilizadas: {technologies.join(", ")}</p>
+      <div className="upper">
+        <h3>{title}</h3>
+        <FlippingCard imageSrc={image} description={description} />
+      </div>
+      <div className="project-tech">
+        <p>Tecnologías utilizadas:</p>
+        <ul>
+          {technologies.map((technology, index) => (
+            <li key={index}>{technology}</li>
+          ))}
+        </ul>
+      </div>
       <a className="btn-primary" href={link} target="_blank" rel="noreferrer">
         Código Fuente
       </a>
