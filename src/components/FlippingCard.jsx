b86114e541d4af7051cab78a2f7b863a0cdc58f2
Alejandro Marin <alexword1893@gmail.com>
2023-10-14 02:47:40 -0300

    fix something wrong with the filter animation

--- src/components/FlippingCard.jsx
+++ src/components/FlippingCard.jsx
@@ -1,17 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/components/FlippingCard.css";
 
 const FlippingCard = ({ imageSrc, description }) => {
   const [showDescription, setShowDescription] = useState(false);
-
-  const handleTouch = () => {
-    setShowDescription(!showDescription);
-  };
+ 
   return (
     <div
       className={`card-container  ${showDescription ? "flipped" : ""}`}
-      onTouchStart={handleTouch}
-      onTouchEnd={handleTouch}
+    
     >
       <div className="card-inner">
         <div className="card-front">
